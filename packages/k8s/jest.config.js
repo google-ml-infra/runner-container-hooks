38f4307d34b30bacdf52399e671269af6f11ee71
Quoc Truong <quoct@google.com>
2025-06-02 21:37:32 -0700

    Fix tests

--- packages/k8s/jest.config.js
+++ packages/k8s/jest.config.js
@@ -5,8 +5,9 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/*-test.ts'],
   testRunner: 'jest-circus/runner',
+  transformIgnorePatterns: ['/node_modules/(?!(@kubernetes/client-node|openid-client|oauth4webapi))'],
   transform: {
-    '^.+\\.ts$': 'ts-jest'
+    "^.+\\.[tj]s$": "babel-jest",
   },
   setupFilesAfterEnv: ['./jest.setup.js'],
   verbose: true
