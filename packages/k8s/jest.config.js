9c02341da99178c4d084d116ce90f3a5534c6ed5
Quoc Truong <quoct@google.com>
2025-06-03 10:41:04 -0700

    Fix test

--- packages/k8s/jest.config.js
+++ packages/k8s/jest.config.js
@@ -1,13 +1,19 @@
 // eslint-disable-next-line import/no-commonjs
 module.exports = {
   clearMocks: true,
-  moduleFileExtensions: ['js', 'ts'],
+  moduleFileExtensions: ['js', 'json', 'ts', 'tsx', 'jsx', 'node'],
+  preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/*-test.ts'],
   testRunner: 'jest-circus/runner',
-  transformIgnorePatterns: ['/node_modules/(?!(@kubernetes/client-node|openid-client|oauth4webapi|jose))'],
+  transformIgnorePatterns: ['/node_modules/(?!(@kubernetes/client-node|openid-client|oauth4webapi|jose)/)'],
   transform: {
-    "^.+\\.[tj]s$": "babel-jest",
+    '^.+\\.(t|j)sx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.test.json',
+      },
+    ],
   },
   setupFilesAfterEnv: ['./jest.setup.js'],
   verbose: true
