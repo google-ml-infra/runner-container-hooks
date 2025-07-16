af33bba27d9adf061836800923ff983a3a69e781
Quoc Truong <quoct@google.com>
2025-07-16 10:09:44 -0700

    Remove spec

--- packages/k8s/src/k8s/index.ts
+++ packages/k8s/src/k8s/index.ts
@@ -1206,8 +1206,6 @@ export async function createJobSet(
   core.debug('extension is ' + extension.spec!!)
   mergePodSpecWithOptions(podSpec, extension.spec!!)
   core.debug('after merged ' + JSON.stringify(podSpec))
-  podSpec.hostNetwork = true
-  podSpec.dnsPolicy = "ClusterFirstWithHostNet"
 
   return await k8sCustomApi.createNamespacedCustomObject({
     group: 'jobset.x-k8s.io',
