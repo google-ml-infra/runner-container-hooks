aa84f7bbf47526e8f411a5919d0b235f56de0458
Quoc Truong <quoct@google.com>
2025-08-08 13:29:11 -0700

    Add logging

--- packages/k8s/src/k8s/index.ts
+++ packages/k8s/src/k8s/index.ts
@@ -529,6 +529,7 @@ async function getPodPhase(podName: string): Promise<PodPhase> {
     name: podName,
     namespace: namespace()
   })
+  console.log(`pod status ${pod.status}`)
   if (!pod.status?.phase || !podPhaseLookup.has(pod.status.phase)) {
     return PodPhase.UNKNOWN
   }
