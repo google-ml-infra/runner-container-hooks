ed927a05379610ce9107f5a9193de1db5ab04e6c
Quoc Truong <quoct@google.com>
2025-08-18 16:38:37 -0700

    Add debugging

--- packages/k8s/src/k8s/utils.ts
+++ packages/k8s/src/k8s/utils.ts
@@ -312,7 +312,8 @@ export function useScriptExecutor(): boolean {
 }
 
 export function getNumberOfHost(): number {
-  return Number(process.env[ENV_NUMBER_OF_HOSTS]) || 1
+  return 1
+//  return Number(process.env[ENV_NUMBER_OF_HOSTS]) || 1
 }
 
 export enum PodPhase {
