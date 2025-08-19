9f3f8da24b990ff724a70061d759ebfd8e8f3932
Quoc Truong <quoct@google.com>
2025-08-19 10:34:52 -0700

    Add sleep

--- packages/k8s/src/hooks/run-script-step.ts
+++ packages/k8s/src/hooks/run-script-step.ts
@@ -144,6 +144,7 @@ async function runScriptStepInJobSet(
         }
       })
     )
+    await sleep(100000)
     core.info(`syncing workflow pod to runner pod with copyFromPod`)
     // We can just copy from one of the pod.
     await copyFromPod('/__w/_temp/_runner_file_commands', '/home/runner/_work/_temp/_runner_file_commands', pods.items[0].metadata!!.name!!, JOB_CONTAINER_NAME)
