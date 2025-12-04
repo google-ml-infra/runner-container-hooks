9edf567edb74402b0afde76d9fd2e5ce455283f4
Quoc Truong <quoct@google.com>
2025-12-04 10:56:47 -0800

    Add retry logic when creating headless service (#50)
    * Add retry logic when creating headless service
    
    * Fix formatting

--- packages/k8s/src/hooks/prepare-job.ts
+++ packages/k8s/src/hooks/prepare-job.ts
@@ -16,11 +16,11 @@ import {
   prunePods,
   waitForPodPhases,
   getPrepareJobTimeoutSeconds,
-  createHeadlessService,
   extractErrorMessageFromK8sError,
   createJobSet,
   createPodSpec,
-  getPodsFromJobSet
+  getPodsFromJobSet,
+  createHeadlessServiceWithRetry
 } from '../k8s'
 import {
   containerVolumes,
@@ -138,7 +138,7 @@ export async function prepareJob(
 
   if (useScriptExecutor() && getNumberOfHost() === 1) {
     core.debug(`Creating headless service`)
-    await createHeadlessService()
+    await createHeadlessServiceWithRetry()
   }
 
   generateResponseFile(responseFile, args, createdPod, isAlpine)
